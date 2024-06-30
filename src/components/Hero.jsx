91ccbd039557f80c6cb59dd2ecbfa3d39b0e5292
Pranay Mishra <mishrapranay21@gmail.com>
2024-06-30 23:00:34 +0530

    hero-2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,188 +29,192 @@ import Luko from "../../images/Luko.png"
 
 
 const Hero = () => {
-    const box = useRef();
-    useGSAP(() => {
-      
-      var tl5 = gsap.timeline({
-        scrollTrigger: {
-          trigger: ".hero",
-          start: "50% 50%",
-          end: "260% 50%",
-          scrub: 0.1,
-          pin: true,
-        }
-      })
-      const isMobile = window.innerWidth <= 500;
-      const imgWidth = isMobile ? "60%" : "30%";
-  
-      tl5.to(".rotate-div", {
-        rotate: -10,
-        scale: 0.9,
+  const box = useRef();
+  useGSAP(() => {
+
+    var tl5 = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero",
+        start: "50% 50%",
+        end: "260% 50%",
+        scrub: 0.1,
+        pin: true,
+      }
+    })
+    const isMobile = window.innerWidth <= 500;
+    const imgWidth = isMobile ? "60%" : "30%";
+
+    tl5.to(".rotate-div", {
+      rotate: -10,
+      scale: 0.9,
+    }, 'a')
+      .to("#row-div-1", {
+        marginTop: "-45%",
       }, 'a')
-        .to("#row-div-1", {
-          marginTop: "-45%",
-        }, 'a')
-        .to("#row-div-2", {
-          marginTop: "-32%",
-        }, 'a')
-        .to("#row-div-3", {
-          marginTop: "-26%",
-        }, 'a')
-        .to("#row-div-4", {
-          marginTop: "-45%",
-        }, 'a')
-        .to("#row-div-5", {
-          marginTop: "-15%",
-        }, 'a')
-        .to(".overlay-div img", {
-          opacity: "1",
-          width: imgWidth,
-        }, 'a')
-        .to(".overlay-div h4", {
-          opacity: "1",
-  
-        }, 'a')
-        .to(".overlay-div", {
-          backgroundColor: "#8b0a0ac6",
-        }, 'a')
-        .to("#hero-que", {
-          opacity: "0",
-          top:0,
-        }, 'a')
-        .to("#hero-p", {
-          opacity: "0",
-          bottom:0,
-        }, 'a')
-        .to("nav", {
-          opacity: 1,
-        }, 'a')
-    }, { scope: box })
-    return (
-        <>
-          <div id="main" ref={box}>
-            <nav>
-              <div className="lft-nav">
-                <img src={Luko} alt="" />
-              </div>
-              <div className="rt-nav">
-                
-              </div>
-    
-            </nav>
-            <div className="hero">
-              <h3 id="hero-que">Seeking the Best Dog Meals?</h3>
-              <p id="hero-p">Scroll down</p>
-              <div className="content-hero">
-                <div className="rotate-div">
-                  <div id="row-div-1" className="row-div">
-                    <div className="img-div">
-                      <img src={dog1} alt="" />
-                    </div>
-                    <div className="img-div">
-                      <img src={dog2} alt="" />
-    
-    
-                    </div>
-                    <div className="img-div">
-                      <img src={dog3} alt="" />
-  
-    
-                    </div>
-                    <div className="img-div">
-                      <img src={dog4}alt="" />
-                    </div>
-    
-    
-                  </div>
-                  <div id="row-div-2" className="row-div">
-                    <div className="img-div">
-                      <img src={dog6} alt="" />
-                    </div>
-                    <div className="img-div">
-                      <img src={dog7} alt="" />
-    
-    
-                    </div>
-                    <div className="img-div">
-                      <img src={dog6} alt="" />
-    
-    
-                    </div>
-                    <div className="img-div">
-                      <img src={dog4} alt="" />
-                    </div>
-    
-                  </div>
-                  <div id="row-div-3" className="row-div">
-                    <div className="img-div">
-                      <img src={dog5} alt="" />
-                    </div>
-                    <div className="img-div">
-                      <img src={dog4} alt="" />
-    
-    
-                    </div>
-                    <div className="img-div">
-                      <img src={dog6} alt="" />
-    
-    
-                    </div>
-                    <div className="img-div">
-                      <img src={dog1} alt="" />
-                    </div>
-    
-                  </div>
-                  <div id="row-div-4" className="row-div">
-                    <div className="img-div">
-                      <img src={dog8} alt="" />
-                    </div>
-                    <div className="img-div">
-                      <img src={dog2} alt="" />
-    
-    
-                    </div>
-                    <div className="img-div">
-                      <img src={dog4} alt="" />
-    
-    
-                    </div>
-                    <div className="img-div">
-                      <img src={dog9} alt="" />
-                    </div>
-    
-                  </div>
-                  <div id="row-div-5" className="row-div">
-                    <div className="img-div">
-                      <img src={dog1} alt="" />
-                    </div>
-                    <div className="img-div">
-                      <img src={dog2} alt="" />
-    
-    
-                    </div>
-                    <div className="img-div">
-                      <img src={dog9} alt="" />
-    
-    
-                    </div>
-                    <div className="img-div">
-                      <img src={dog6} alt="" />
-                    </div>
-    
-                  </div>
+      .to("#row-div-2", {
+        marginTop: "-32%",
+      }, 'a')
+      .to("#row-div-3", {
+        marginTop: "-26%",
+      }, 'a')
+      .to("#row-div-4", {
+        marginTop: "-45%",
+      }, 'a')
+      .to("#row-div-5", {
+        marginTop: "-15%",
+      }, 'a')
+      .to(".overlay-div img", {
+        opacity: "1",
+        width: imgWidth,
+      }, 'a')
+      .to(".overlay-div h4", {
+        opacity: "1",
+
+      }, 'a')
+      .to(".overlay-div", {
+        backgroundColor: "#8b0a0ac6",
+      }, 'a')
+      .to(".overlay-text p", {
+        opacity: "0",
+        top: "87%",
+      }, 'a')
+      .to("#hero-que ", {
+        opacity: "0",
+        top: 0,
+      }, 'a')
+      
+      .to("nav", {
+        opacity: 1,
+      }, 'a')
+  }, { scope: box })
+  return (
+    <>
+      <div id="main" ref={box}>
+        <nav>
+          <div className="lft-nav">
+            <img src={Luko} alt="" />
+          </div>
+          <div className="rt-nav">
+
+          </div>
+
+        </nav>
+        <div className="hero">
+          <div className="overlay-text">
+            <h3 id="hero-que">Seeking the Best Dog Meals?</h3>
+            <p id="hero-p">Scroll down</p>
+          </div>
+
+          <div className="content-hero">
+            <div className="rotate-div">
+              <div id="row-div-1" className="row-div">
+                <div className="img-div">
+                  <img src={dog1} alt="" />
                 </div>
-                <div className="overlay-div">
-                  <img src={Luko} alt="" />
-                  <h4>The Balance Your Buddy Deserves</h4>
+                <div className="img-div">
+                  <img src={dog2} alt="" />
+
+
                 </div>
+                <div className="img-div">
+                  <img src={dog3} alt="" />
+
+
+                </div>
+                <div className="img-div">
+                  <img src={dog4} alt="" />
+                </div>
+
+
+              </div>
+              <div id="row-div-2" className="row-div">
+                <div className="img-div">
+                  <img src={dog9} alt="" />
+                </div>
+                <div className="img-div">
+                  <img src={dog7} alt="" />
+
+
+                </div>
+                <div className="img-div">
+                  <img src={dog6} alt="" />
+
+
+                </div>
+                <div className="img-div">
+                  <img src={dog4} alt="" />
+                </div>
+
+              </div>
+              <div id="row-div-3" className="row-div">
+                <div className="img-div">
+                  <img src={dog5} alt="" />
+                </div>
+                <div className="img-div">
+                  <img src={dog4} alt="" />
+
+
+                </div>
+                <div className="img-div">
+                  <img src={dog3} alt="" />
+
+
+                </div>
+                <div className="img-div">
+                  <img src={dog1} alt="" />
+                </div>
+
+              </div>
+              <div id="row-div-4" className="row-div">
+                <div className="img-div">
+                  <img src={dog8} alt="" />
+                </div>
+                <div className="img-div">
+                  <img src={dog2} alt="" />
+
+
+                </div>
+                <div className="img-div">
+                  <img src={dog1} alt="" />
+
+
+                </div>
+                <div className="img-div">
+                  <img src={dog9} alt="" />
+                </div>
+
+              </div>
+              <div id="row-div-5" className="row-div">
+                <div className="img-div">
+                  <img src={dog1} alt="" />
+                </div>
+                <div className="img-div">
+                  <img src={dog2} alt="" />
+
+
+                </div>
+                <div className="img-div">
+                  <img src={dog9} alt="" />
+
+
+                </div>
+                <div className="img-div">
+                  <img src={dog6} alt="" />
+                </div>
+
               </div>
             </div>
-            
+            <div className="overlay-div">
+              <img src={Luko} alt="" />
+              <h4>The Balance Your Buddy Deserves</h4>
+            </div>
           </div>
-    
-        </>
-      )
+        </div>
+
+      </div>
+
+    </>
+  )
 }
 
 export default Hero
